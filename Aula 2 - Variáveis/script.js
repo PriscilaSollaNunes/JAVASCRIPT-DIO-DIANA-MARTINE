6acc083d35c2c16ada7041e53e37a206fc8d4f7f
Seu nome <priscilasolla.ps@gmail.com>
2022-08-08 16:44:04 -0300

    add all files

---
+++ Aula 2 - Variáveis/script.js
@@ -0,0 +1,110 @@
+// tipos primitivos 
+
+// boolean
+/* var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof (numeroQualquer));
+
+// string
+var nome = "Priscila";
+console.log(typeof (nome)) */
+
+// como declarar 
+/* var variavel = 'Priscila';
+variavel = 'Solla';
+console.log(variavel);*/
+
+/*let variavel2 = 'priscila';
+variavel2 = 'Solla';
+console.log(variavel2);*/
+
+/* const contante = 'Priscila';
+console.log (contante)*/
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno)
+}
+
+escopoLocal();*/
+
+/*// atribuição 
+var atribuicao = 'Priscila';
+
+// comparação 
+var comparacao = '0' == 0;
+console.log (comparacao);
+
+// comparação identica
+var comparacaoIdentica = '0' === 0;
+console.log (comparacaoIdentica);
+*/
+
+/*// adição
+var adicao = 1 + 1;
+console.log(adicao);*/
+
+/*// subtracao
+var subtracao = 2 - 1;
+console.log(subtracao); */
+
+/* // mutiplicacao
+var mutiplicacao = 2 * 3;
+console.log(mutiplicacao); */
+
+/* // divisao real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);*/
+
+/* // divisao inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira); */
+
+/* // potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao); */
+
+/* // maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue); */
+
+/* // menor que
+var menorQue = 5 < 2;
+console.log(menorQue); */
+
+/* // maior ou igual
+var maiorOuIgual = 5 >= 2; 
+console.log(maiorOuIgual); */ 
+
+/* // menor ou igual 
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);  */
+
+/* var e = true && true;
+console.log(e); */ 
+
+/* var ou = false|| false;
+console.log(ou); */
+
+var nao = !true;
+console.log(nao);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
